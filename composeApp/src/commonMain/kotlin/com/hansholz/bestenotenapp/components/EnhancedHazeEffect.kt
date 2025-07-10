8f3cab11bddbbfc85b4f2f285a37ee34e9d95819
HansHolz09 <Istdaniemand8@gmail.com>
2025-07-10 10:54:51 +0200

    Added blurScale to EnhancedHazeEffect.kt

--- composeApp/src/commonMain/kotlin/com/hansholz/bestenotenapp/components/EnhancedHazeEffect.kt
+++ composeApp/src/commonMain/kotlin/com/hansholz/bestenotenapp/components/EnhancedHazeEffect.kt
@@ -5,16 +5,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.hansholz.bestenotenapp.main.Platform
+import com.hansholz.bestenotenapp.main.getPlatform
 import com.hansholz.bestenotenapp.theme.LocalBlurEnabled
-import dev.chrisbanes.haze.*
+import dev.chrisbanes.haze.ExperimentalHazeApi
+import dev.chrisbanes.haze.HazeEffectScope
+import dev.chrisbanes.haze.HazeInputScale
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.HazeTint
+import dev.chrisbanes.haze.hazeEffect
 
 @OptIn(ExperimentalHazeApi::class)
 @Composable
 fun Modifier.enhancedHazeEffect(hazeState: HazeState? = null, color: Color? = null, blurRadius: Dp? = null, block: (HazeEffectScope.() -> Unit)? = null): Modifier {
     val blurEnabled = LocalBlurEnabled.current.value
+    val blurScale = if (getPlatform() == Platform.ANDROID) 3 else 1
     return this.hazeEffect(hazeState) {
         this.blurEnabled = blurEnabled
-        this.blurRadius = blurRadius ?: 10.dp
+        this.blurRadius = (blurRadius ?: 10.dp) * blurScale
         color?.let {
             backgroundColor = it
             fallbackTint = HazeTint(it)
