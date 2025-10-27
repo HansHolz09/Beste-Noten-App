c6d9be76910d30a2a566a9f7768ebfd2d983f564
HansHolz09 <Istdaniemand8@gmail.com>
2025-10-27 14:02:25 +0100

    Fixed crash on Android and Desktop by changing KTor-Engines

--- composeApp/src/androidMain/kotlin/com/hansholz/bestenotenapp/api/HttpClient.android.kt
+++ composeApp/src/androidMain/kotlin/com/hansholz/bestenotenapp/api/HttpClient.android.kt
@@ -1,9 +1,9 @@
 package com.hansholz.bestenotenapp.api
 
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.okhttp.OkHttp
 
 actual fun createHttpClient() =
-    HttpClient(CIO) {
+    HttpClient(OkHttp) {
         commonHttpClientConfig(this)
     }
--- composeApp/src/desktopMain/kotlin/com/hansholz/bestenotenapp/api/HttpClient.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/hansholz/bestenotenapp/api/HttpClient.desktop.kt
@@ -1,9 +1,9 @@
 package com.hansholz.bestenotenapp.api
 
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.apache5.Apache5
 
 actual fun createHttpClient() =
-    HttpClient(CIO) {
+    HttpClient(Apache5) {
         commonHttpClientConfig(this)
     }
