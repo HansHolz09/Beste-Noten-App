94141fc6abd70ab8bf60e5099634041db7aeaeba
HansHolz09 <Istdaniemand8@gmail.com>
2025-12-25 09:55:35 +0100

    Improved Material You on Desktop

--- composeApp/src/desktopMain/kotlin/com/hansholz/bestenotenapp/theme/Theme.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/hansholz/bestenotenapp/theme/Theme.desktop.kt
@@ -2,10 +2,18 @@ package com.hansholz.bestenotenapp.theme
 
 import androidx.compose.material3.ColorScheme
 import androidx.compose.runtime.Composable
+import com.materialkolor.dynamiccolor.ColorSpec
+import com.materialkolor.rememberDynamicColorScheme
 import dev.zwander.compose.rememberThemeInfo
 
 @Composable
 internal actual fun SystemAppearance(
     isDark: Boolean,
     customColorScheme: @Composable (ColorScheme?) -> Unit,
-) = customColorScheme(rememberThemeInfo(isDark).colors)
+) = customColorScheme(
+    rememberDynamicColorScheme(
+        seedColor = rememberThemeInfo(isDark).seedColor,
+        isDark = isDark,
+        specVersion = ColorSpec.SpecVersion.SPEC_2025,
+    ),
+)
